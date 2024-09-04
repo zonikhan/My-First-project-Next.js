49ca1b6954bb4ae39d141b6f3f72033937d88f85
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-09-04 15:43:23 +0500

    my first project

---
+++ page.tsx
@@ -0,0 +1,13 @@
+export default function home(){
+  return (
+    // <h1 className="text-pink-500">
+    // this is rabia
+    // </h1>
+    <h1 className="text-red-500"> Hellow world</h1>
+
+    
+      
+    
+  );
+}
+
